683c0556c9da584265ef5193b4fc988c892fb2e9
asfandtaimoor <asfandtaimoor786@gmail.com>
2025-02-09 16:25:37 +0500

    DONE : User Modal Convert to modules

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
   class User extends Model {
     /**
      * Helper method for defining associations.
