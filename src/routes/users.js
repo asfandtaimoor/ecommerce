f5ba1eb468084f742c6d7124c436a110740a56de
asfandtaimoor <asfandtaimoor786@gmail.com>
2025-02-09 17:20:55 +0500

    DONE : Crud User setup

--- src/routes/users.js
+++ src/routes/users.js
@@ -19,6 +19,61 @@ router.get('/', async (req, res) => {
   }
 });
 
+// ✅ Create User
+router.post('', async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    // Check if user already exists
+    const [existingUser] = await db
+      .promise()
+      .query('SELECT * FROM Users WHERE email = ?', [email]);
+
+    if (existingUser.length > 0) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const [result] = await db
+      .promise()
+      .query(
+        'INSERT INTO Users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)',
+        [firstName, lastName, email, password]
+      );
+
+    res
+      .status(201)
+      .json({ message: 'User created successfully', userId: result.insertId });
+  } catch (err) {
+    console.error('Error creating user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// ✅ Update User
+router.put('/:id', async (req, res) => {
+  const userId = req.params.id;
+  const { firstName, lastName, password } = req.body;
+
+  try {
+    const [results] = await db
+      .promise()
+      .query(
+        'UPDATE Users SET firstName = ?, lastName = ?,   password = ? WHERE id = ?',
+        [firstName, lastName, password, userId]
+      );
+
+    console.log(results);
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User updated successfully' });
+  } catch (err) {
+    console.error('Error updating user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // ✅ Delete a user by ID
 router.delete('/:id', async (req, res) => {
   const userId = req.params.id;
